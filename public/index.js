d962521aaf99a7e44b29e594636d7996b5d3e603
ggakila <gisorehaggai@gmail.com>
2023-05-23 07:16:44 +0300

    projects and contact sections added

--- public/index.js
+++ public/index.js
@@ -1,6 +1,13 @@
 const menu = document.querySelector(".menu")
 const menubutton = document.querySelector(".menubutton")
+const menulinks = document.querySelectorAll(".menulink")
 
 menubutton.addEventListener("click", ()=>{
     menu.classList.toggle("hidden")
 })
+
+ menulinks.forEach(menulink => {
+     menulink.addEventListener("click", ()=>{
+         menu.classList.toggle("hidden")
+     })
+ });
